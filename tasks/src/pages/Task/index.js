55e781d7c058c4ef9e2b766be0727aff4fc80b10
lghiggino <lghiggino@gmail.com>
2021-10-12 17:18:46 -0300

    items are fetched and rendered

--- tasks/src/pages/Task/index.js
+++ tasks/src/pages/Task/index.js
@@ -6,33 +6,85 @@ import {
     TouchableOpacity,
     FlatList
 } from "react-native"
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-
-import styles from './style'
-
+import { collection, getDocs } from 'firebase/firestore/lite';
 import database from '../../config/firebaseconfig'
+//estilos e icones
+import styles from './style'
+import { FontAwesome } from '@expo/vector-icons'
 
 
-export default function Task() {
+
+
+export default function Task({ navigation }) {
     const [tasks, setTasks] = useState([])
 
     async function getTasks(database) {
+        const list = []
         const taskCol = collection(database, 'tasks');
         const taskSnapshot = await getDocs(taskCol);
-        const taskList = taskSnapshot.docs.map(doc => doc.data());
-        setTasks(taskList);
+        taskSnapshot.forEach(task => {
+            list.push({ ...task.data(), id: task.id })
+        })
+        setTasks(list);
     }
 
     useEffect(() => {
-        (async() => {
-            await getTasks(database)
-        })()
+        getTasks(database)
     }, [])
 
+
+    async function deleteTask(id) {
+        await database.collection('tasks').doc(id).delete()
+    }
+
     return (
-        <View>
-            <Text>Hello from Task</Text>
-            <Text>{JSON.stringify(tasks, null, 2)}</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            {tasks &&
+                <FlatList
+                    showsVerticalScrollIndicator={false}
+                    data={tasks}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => {
+                        return (
+                            <View style={styles.tasks}>
+                                <TouchableOpacity
+                                    style={styles.deleteTask}
+                                    onPress={() => {
+                                        deleteTask(item.id)
+                                    }}
+                                >
+                                    <FontAwesome
+                                        name='trash'
+                                        size={23}
+                                        color='#f92e6a'
+                                    />
+                                </TouchableOpacity>
+                                <Text style={styles.description}
+                                    onPress={() => {
+                                        navigation.navigate('Details', {
+                                            id: item.id,
+                                            description: item.description
+                                        })
+                                    }}
+                                >
+                                    {item.description}
+                                </Text>
+                            </View>
+                        )
+                    }}
+                />
+            }
+
+            <TouchableOpacity
+                style={styles.buttonNewTask}
+                onPress={() => { navigation.navigate('NewTask') }}
+            >
+                <Text style={styles.iconButton}>
+                    +
+                </Text>
+            </TouchableOpacity>
+
+           
+        </SafeAreaView>
     )
 }
--- tasks/src/pages/Task/style.js
+++ tasks/src/pages/Task/style.js
@@ -1,7 +1,48 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-
+    container:{
+        flex: 1,
+        backgroundColor: '#aaa',
+        paddingTop: 10
+    },
+    buttonNewTask:{
+        position: 'absolute',
+        width: 60,
+        height: 60,
+        backgroundColor:'#f92e6a',
+        bottom:30,
+        left:20,
+        borderRadius:50,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    iconButton:{
+        color: '#fff',
+        fontSize: 25,
+        fontWeight: 'bold'
+    },
+    tasks:{
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 5
+    },
+    deleteTask:{
+        justifyContent: 'center',
+        paddingLeft: 20
+    },
+    description:{
+        width: '75%',
+        alignContent: 'flex-start',
+        backgroundColor: '#f5f5f5',
+        padding: 10,
+        paddingHorizontal: 20,
+        borderRadius: 50,
+        marginBottom: 5,
+        marginRight: 15,
+        color: '#666'
+    }
 })
 
 export default styles

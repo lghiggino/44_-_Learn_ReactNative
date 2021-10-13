f3d6d4c188132e4b6502fb62c6e6a37621292233
lghiggino <lghiggino@gmail.com>
2021-10-13 20:37:00 -0300

    usando {route} e route.params.description em novo componente

--- tasks/src/pages/Details/index.js
+++ tasks/src/pages/Details/index.js
@@ -1,14 +1,57 @@
-import React from "react";
-import {SafeAreaView, View, Text } from "react-native"
+import React, { useState } from "react";
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity } from "react-native"
+
+//Firebase
+import { collection, getDocs, query, where, doc, setDoc } from 'firebase/firestore/lite';
+import database from '../../config/firebaseconfig'
 
 //STYLES
 import styles from './style'
+import { AntDesign } from '@expo/vector-icons';
 
 
-export default function Details() {
+export default function Details({navigation, route}) {
+    const [descriptionEdit, setDescriptionEdit] = useState(route.params.description)
+    const [taskId, setTaskId] = useState(route.params.id)
+
+    function editTask(){
+        console.log(taskId)
+    }
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Hello from Details</Text>
+            <Text style={styles.label}>Edit Task</Text>
+            <TextInput
+                style={styles.textInput}
+                onChangeText={(text) => { setDescriptionEdit(text) }}
+                value={descriptionEdit}
+                multiline
+            />
+            <TouchableOpacity style={styles.buttonNewTask}>
+                <Text
+                    onPress={() => { editTask() }}
+                    style={styles.buttonNewTaskText}
+                >
+                    Edit Task
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={styles.buttonBack}
+            >
+                <Text
+                    style={styles.iconButton}
+                    onPress={() => {
+                        navigation.popToTop()
+                    }}
+                >
+                    <AntDesign
+                        name="arrowleft"
+                        size={23}
+                        color="#fff"
+                    />
+                </Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }

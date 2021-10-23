48e979ec5ba2203d00acbfea980efec024f450d4
lghiggino <lghiggino@gmail.com>
2021-10-23 19:51:07 -0300

    StyleMaker function baby!

--- multitasks/src/components/Button.js
+++ multitasks/src/components/Button.js
@@ -1,14 +1,55 @@
+import React from "react";
 import { TouchableOpacity, Text, StyleSheet, Platform } from "react-native";
 
-export default function Button(){
-    const {buttonText, onPress, ...rest} = props
-    console.log(props)
-    return(
+export default function Button(props) {
+    const { buttonText, onPress, primary, success, color, size, shape, ...rest } = props
+
+    const styleMaker = () => {
+        console.log("from StyleCreator")
+        const inlineStyle = {
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingVertical: 15,
+            borderRadius: 4,
+            elevation: 3,
+        }
+        switch (props.color) {
+            case "primary":
+                inlineStyle.backgroundColor = '#f92e6a'
+                break
+            case "success":
+                inlineStyle.backgroundColor = '#4caf50'
+                break
+            case "danger":
+                inlineStyle.backgroundColor = '#f44336'
+                break
+            default:
+                inlineStyle.backgroundColor = '#999'
+        }
+        switch (props.size) {
+            case "small":
+                inlineStyle.maxWidth = "25%"
+                break
+            case "medium":
+                inlineStyle.maxWidth = "50%"
+                break
+            case "large":
+                inlineStyle.maxWidth = "75%"
+                break
+        }
+        //switch(props.shape){}
+        console.log(inlineStyle)
+        return inlineStyle
+    }
+
+    return (
         <TouchableOpacity
             onPress={onPress}
-            style={}
+            style={styleMaker()}
         >
-            <Text>
+            <Text
+                style={styles.textLight}
+            >
                 {buttonText}
             </Text>
         </TouchableOpacity>
@@ -16,19 +57,38 @@ export default function Button(){
 }
 
 
-
 const styles = StyleSheet.create({
-    //colors:
-    primary:{
-        backgroundColor: '#f92e6a'
+    default: {
+        backgroundColor: '#999',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        maxWidth: 120
     },
-    textPrimary:{
-        color: '#f5f5f5'
+    primary: {
+        backgroundColor: '#f92e6a',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        maxWidth: 120
     },
-    light:{
-        backgroundColor: '#fff'
+    success: {
+        backgroundColor: '#4caf50',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        maxWidth: 120
     },
-    textDark:{
-        color: '#333'
+    textLight: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: '#f5f5f5',
+        borderColor: 'red',
+        borderStyle: "solid",
+        borderWidth: 2
+    },
+    textDark: {
+        color: '#333',
+        // marginLeft: 'auto',
+        // marginRight: 'auto'
     }
 })
--- multitasks/src/pages/Components/index.js
+++ multitasks/src/pages/Components/index.js
@@ -12,7 +12,29 @@ import Button from "../../components/Button";
 export default function Components({ navigation, route }) {
     return (
         <SafeAreaView style={styles.container}>
-            <Button buttonText={"click me"} onPress={() => {console.log("hello")}} />
+            <Button
+                buttonText={"size = small"}
+                onPress={() => { console.log("") }}
+                color="success"
+                size="small"
+            />
+            <Button
+                buttonText={"size = medium"}
+                onPress={() => { console.log("hello") }}
+                color="danger"
+                size="medium"
+            />
+            <Button
+                buttonText={"size = large"}
+                onPress={() => { console.log("hello") }}
+                color="success"
+                size="large"
+            />
+            <Button
+                buttonText={"without size prop"}
+                onPress={() => { console.log("hello") }}
+                color="danger"
+            />
         </SafeAreaView>
     )
 }

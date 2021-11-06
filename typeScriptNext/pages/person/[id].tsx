12b6de55020201a3f80d694959ee88677c1aed5e
lghiggino <lghiggino@gmail.com>
2021-11-06 08:54:19 -0300

    attempting getStaticPaths in person/[id]

--- typeScriptNext/pages/person/[id].tsx
+++ typeScriptNext/pages/person/[id].tsx
@@ -1,8 +1,37 @@
 import React from "react"
+import axios from "axios"
 import PersonScreen from "../../screens/person"
+import { GetStaticPaths, GetStaticProps } from "next"
 
-export default function PersonPage(){
-    return(
-        <PersonScreen />
+
+const getStaticPaths: GetStaticPaths = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users")
+    const data = await res.data
+
+    const paths = data.map(person => {
+        return {
+            params: { id: person.id.toString() }
+        }
+    })
+
+    return {
+        paths: paths,
+        fallback: false
+    }
+}
+const getStaticProps: GetStaticProps = async (context) => {
+    const id = context.params.id
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users" + id)
+    const data = await res.data
+    console.log("data from getStaticProps", data)
+
+    return {
+        props: { person: data }
+    }
+}
+
+export default function PersonPage({ person }) {
+    return (
+        <PersonScreen person={person} />
     )
 }
--- typeScriptNext/screens/person.tsx
+++ typeScriptNext/screens/person.tsx
@@ -1,24 +1,43 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Platform, View, Text } from "react-native";
 import { useRouting } from 'expo-next-react-navigation'
 
-export default function PersonScreen(){
+export default function PersonScreen({ person }) {
     const { navigate, push, getParam, goBack } = useRouting()
 
     const id = getParam('id')
     const name = getParam('name')
     const email = getParam('email')
 
-
     console.log(id, name, email)
 
-    return(
+    const [response, setResponse] = useState(null)
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(true)
+
+    async function getAxiosUsers() {
+        try {
+            const usersFromAxios = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+            setResponse(usersFromAxios.data)
+        } catch (err) {
+            setError(err)
+        }
+    }
+
+    useEffect(() => {
+        Platform.OS === 'web' ? setResponse(person) : getAxiosUsers()
+        console.log("response", response)
+    }, [])
+
+
+
+
+    return (
         <View>
             <Text>{id}</Text>
-            <Text>Name:</Text>
-            <Text>{name}</Text>
-            <Text>Email:</Text>
-            <Text>{email}</Text>
+            <Text>Name: {name}</Text>
+            <Text>Email: {email}</Text>
         </View>
     )
 }

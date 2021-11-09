a64296839cef34e198ef05daba01e9f3a3afe74c
lghiggino <lghiggino@gmail.com>
2021-11-08 21:57:41 -0300

    authExpoWithTS first commit till Lannister

---
+++ authExpoTypeScript/src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Dashboard: React.FC = () => {
+    return (
+        <View>
+            <Text>Hello from Dashboard</Text>
+        </View>
+    )
+};
+
+export default Dashboard;
---
+++ authExpoTypeScript/src/pages/SignIn/index.tsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { View, Button, StyleSheet } from "react-native";
+import AuthContext from "../../contexts/auth";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
+
+const SignIn: React.FC = () => {
+  const {signed, signIn, user} = useContext(AuthContext);
+  console.log(signed);
+  console.log(user);
+
+  function handleSign() {
+    signIn();
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Sign In" onPress={handleSign} />
+    </View>
+  );
+};
+
+export default SignIn;

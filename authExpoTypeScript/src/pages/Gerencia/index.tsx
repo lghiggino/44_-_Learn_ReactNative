46089af6d7ec274967c04aa313539a4056653cb5
lghiggino <lghiggino@gmail.com>
2021-11-10 11:11:49 -0300

    baseline commit - working as expected with useContext

--- authExpoTypeScript/src/pages/Gerencia/index.tsx
+++ authExpoTypeScript/src/pages/Gerencia/index.tsx
@@ -1,18 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { View, Text, Button } from "react-native";
 import AuthContext from "../../contexts/auth";
 
-export default function UserArea({ navigation }) {
+export default function Gerencia({ navigation }) {
     const { user, signOut } = useContext(AuthContext);
 
     const handleSignOut = () => {
         signOut();
     }
 
+    const loadData= (user) => {
+        //aqui entra o axios puxando os dados e passando user.password
+    }
+
     return (
         <>
             <View>
-                <Text>Hello from UserArea</Text>
+                <Text>Hello from Gerencia</Text>
                 <Text>{JSON.stringify(user, null, 2)}</Text>
             </View>
             <View>
--- authExpoTypeScript/src/routes/app.routes.tsx
+++ authExpoTypeScript/src/routes/app.routes.tsx
@@ -3,12 +3,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Dashboard from '../pages/Dashboard';
 import UserArea from '../pages/UserArea';
+import Gerencia from '../pages/Gerencia';
 
 const AppStack = createNativeStackNavigator();
 
 const AppRoutes: React.FC = () => (
   <AppStack.Navigator>
     <AppStack.Screen name="Dashboard" component={Dashboard} />
+    <AppStack.Screen name="Gerencia" component={Gerencia} />
     <AppStack.Screen name="UserArea" component={UserArea} />
   </AppStack.Navigator>
 );

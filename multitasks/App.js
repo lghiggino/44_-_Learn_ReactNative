06884922d284f1b70433a1c01669f8488eb19c62
lghiggino <lghiggino@gmail.com>
2021-11-11 08:40:14 -0300

    Login no topo da navegação

--- multitasks/App.js
+++ multitasks/App.js
@@ -16,7 +16,7 @@ const Stack = createStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Components'>
+      <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen
           name='Login'
           component={Login}

a6dde47c46052b85ed8b50112074d290b5463cb0
lghiggino <lghiggino@gmail.com>
2021-11-12 08:06:57 -0300

    FlatList vs Scrollview experiment

--- authExpoTypeScript/src/pages/Dashboard/index.tsx
+++ authExpoTypeScript/src/pages/Dashboard/index.tsx
@@ -1,31 +1,116 @@
-import React, { useContext } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { StyleSheet, Text, FlatList, SafeAreaView, ScrollView, View, StatusBar, Button } from "react-native";
 import AuthContext from "../../contexts/auth";
 
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
     },
-});
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+    },
+];
+
+const Item = ({ title }) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
 
 export default function Dashboard({ navigation }) {
     const { user, signOut } = useContext(AuthContext);
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(
+        {
+            state: false,
+            message: ""
+        }
+    )
 
     function handleSignOut() {
         signOut();
     }
 
+    const renderItem = ({ item }) => (
+        <Item title={item.title} />
+    );
+
     return (
-        <View style={styles.container}>
-            <Text>email: {user?.email}</Text>
-            <Text>password: {user?.password}</Text>
-            <Button title="Sign Out" onPress={handleSignOut} />
-            <Button
-                title="Go to UserArea"
-                onPress={() => navigation.navigate('UserArea')}
+        // <ScrollView>
+        //     <View >
+        //         <Text>email: {user?.email}</Text>
+        //         <Text>password: {user?.password}</Text>
+        //     </View>
+        //     <View >
+        //         <Text>Meiuca</Text>
+        //     </View>
+        //     <View>
+        //         <Button title="Sign Out" onPress={handleSignOut} />
+        //         <Button
+        //             title="Go to UserArea"
+        //             onPress={() => navigation.navigate('UserArea')}
+        //         />
+        //     </View>
+        // </ScrollView>
+        <SafeAreaView style={styles.container}>
+            
+            <ScrollView style={styles.scrollView}>
+            <View>
+                <Button title="Sign Out" onPress={handleSignOut} />
+                <Button
+                    title="Go to UserArea"
+                    onPress={() => navigation.navigate('UserArea')}
+                />
+            </View>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </Text>
+            </ScrollView>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
             />
-        </View>
+        </SafeAreaView>
     );
 };
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight / 2 || 0,
+    },
+    scrollView: {
+        backgroundColor: 'pink',
+        marginHorizontal: 10,
+    },
+    text: {
+        fontSize: 42,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 10,
+    },
+    title: {
+        fontSize: 32,
+    },
+});
+

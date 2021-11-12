3a733fa87f52ad5bd554d698f5712f1b2876eda1
lghiggino <lghiggino@gmail.com>
2021-11-12 08:38:06 -0300

    decoupled Post Component into new file

---
+++ authExpoTypeScript/src/pages/SinglePost/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function SinglePost(){
+    return(
+        <View>
+            <Text>Hello from Single Post</Text>
+        </View>
+    )
+}
--- authExpoTypeScript/src/routes/app.routes.tsx
+++ authExpoTypeScript/src/routes/app.routes.tsx
@@ -3,15 +3,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Dashboard from '../pages/Dashboard';
 import UserArea from '../pages/UserArea';
+import SinglePost from '../pages/SinglePost';
 
 
 const AppStack = createNativeStackNavigator();
 
 const AppRoutes: React.FC = () => (
-  <AppStack.Navigator>
-    <AppStack.Screen name="Dashboard" component={Dashboard} />
-    <AppStack.Screen name="UserArea" component={UserArea} />
-  </AppStack.Navigator>
+    <AppStack.Navigator>
+        <AppStack.Screen name="Dashboard" component={Dashboard} />
+        <AppStack.Screen name="UserArea" component={UserArea} />
+        <AppStack.Screen name="SinglePost" component={SinglePost} />
+    </AppStack.Navigator>
 );
 
 export default AppRoutes;

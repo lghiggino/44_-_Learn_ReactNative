6500b7ceb490b55227130aa84ac503fc389b2b20
lghiggino <lghiggino@gmail.com>
2021-11-13 08:39:26 -0300

    post component

--- authExpoTypeScript/src/components/Post.tsx
+++ authExpoTypeScript/src/components/Post.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native'
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export const Post = ({ id, title, body }) => (
-    <TouchableOpacity>
+export const Post = ({ id, title, body, navigation }) => (
+    <TouchableOpacity 
+        onPress={() => {navigation.navigate("SinglePost", {
+            id,
+            title,
+            body
+        })}}
+    >
         <View style={styles.item}>
             <Text style={styles.title}>{title}</Text>
             <Text style={styles.body}>{body}</Text>

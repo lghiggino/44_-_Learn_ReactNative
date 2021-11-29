5fa38faef462c621bbd4fee4a0ccba248a987c14
lghiggino <lghiggino@gmail.com>
2021-11-29 09:58:25 -0300

    passing params via routes

--- ExpoNextMonorepo/packages/app/artists.tsx
+++ ExpoNextMonorepo/packages/app/artists.tsx
@@ -1,17 +1,30 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { LinkToRouteName } from './utils/CustomLinks'
+import Card from '../expo/components/templates/Card'
+import { Link, useRouting } from "expo-next-react-navigation"
 
 
-export function Artists({ }) {
+
+export function Artists({ route }) {
+  const { getParam } = useRouting()
+  const id = getParam('id')
+  console.log("id is: ",id)
+
+
   return (
-    <View style={styles.container}>
-      <Text>I'm artists page, baybay</Text>
-      <View style={styles.button}>
-        <LinkToRouteName routeName="hello" webpath="hello" >
-          <Text style={styles.buttonText}>Voltar para Hello</Text>
-        </LinkToRouteName>
+    <>
+      <View style={styles.container}>
+        <Text>I'm artists page, baybay</Text>
+        <View style={styles.button}>
+          <LinkToRouteName routeName="hello" webpath="hello" >
+            <Text style={styles.buttonText}>Voltar para Hello</Text>
+          </LinkToRouteName>
+        </View>
       </View>
-    </View>
+
+      {/* <Card key={item.id} title={item.name} source={'https://picsum.photos/200'} subtitle={item.email} onPress={alert(item.city)} callToAction='say Hello' /> */}
+
+    </>
   )
 }
 
@@ -21,8 +34,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  buttonText: { 
-    color: 'white' 
+  buttonText: {
+    color: 'white'
   },
   button: {
     paddingHorizontal: 12,
@@ -32,3 +45,4 @@ const styles = StyleSheet.create({
     marginVertical: 5
   }
 })
+

9593b38c9d5ee4ba6a3aafb3df0e21f754f4ec79
lghiggino <lghiggino@gmail.com>
2021-12-01 21:51:38 -0300

    fix dos estilos para web

--- ExpoNextMonorepo/packages/app/artists.tsx
+++ ExpoNextMonorepo/packages/app/artists.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Platform, Dimensions, View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { LinkToRouteName } from './utils/CustomLinks'
 import Card from '../expo/components/templates/Card'
 import { Link, useRouting } from "expo-next-react-navigation"
@@ -23,14 +23,14 @@ type IItem = {
   "email": string,
   "id": number,
   "name": string,
-  "phone":string,
+  "phone": string,
   "username": string,
   "website": string
 
 }
 
 
-export function Artists({ route }) {
+export function Artists() {
   const { getParam } = useRouting()
   const item: IItem = getParam('item')
   console.log("item is", item)
@@ -45,8 +45,32 @@ export function Artists({ route }) {
             <Text style={styles.buttonText}>Voltar para Hello</Text>
           </LinkToRouteName>
         </View>
-        <Text>{item.name}</Text>
-        <Text>{item.email}</Text>
+        <View style={styles.card}>
+          <View style={styles.cardBackgroundImage}></View>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.cardAvatarImage}></View>
+            <View style={styles.cardAvatarLettering}></View>
+          </View>
+          <View style={styles.cardButtonContainer}>
+            <View style={styles.cardButton}><Text style={styles.cardButtonText}>Follow</Text></View>
+            <View style={styles.cardButton}><Text style={styles.cardButtonText}>Message</Text></View>
+          </View>
+          <View style={styles.cardFooter}>
+            <View style={styles.cardFooterFollowersText}></View>
+            <View style={styles.cardFooterFollowersAvatars}>
+              <View style={styles.cardFooterFollowersAvatarsImages}></View>
+              <View style={styles.cardFooterFollowersAvatarsImagesExtra}></View>
+            </View>
+          </View>
+
+        </View>
+        {item &&
+          <>
+            <Text>{item.name}</Text>
+            <Text>{item.email}</Text>
+          </>
+        }
+
       </View>
 
       {/* <Card key={item.id} title={item.name} source={'https://picsum.photos/200'} subtitle={item.email} onPress={alert(item.city)} callToAction='say Hello' /> */}
@@ -55,6 +79,7 @@ export function Artists({ route }) {
   )
 }
 
+const { width } = Dimensions.get('screen')
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -70,6 +95,74 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     borderRadius: 6,
     marginVertical: 5
-  }
+  },
+  card: {
+    width: width - 24,
+    maxWidth: 400,
+    // paddingHorizontal: 10,
+    height: 440,
+    backgroundColor: 'red',
+    borderRadius: 12,
+    flexDirection: 'column'
+  },
+  cardBackgroundImage: {
+    width: '100%',
+    backgroundColor: 'blue',
+    height: 160,
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+  },
+  cardAvatarImage: {
+    width: 120,
+    height: 120,
+    marginLeft: 24,
+    backgroundColor: 'yellow',
+    position: 'relative',
+    bottom: 50,
+    borderRadius: 100
+  },
+  cardAvatarLettering: {
+    width: Platform.OS === "web" ? (400-216) : (width - 216),
+    marginLeft: 24,
+    height: 70,
+    backgroundColor: 'green'
+  },
+  cardButtonContainer: {
+    flexDirection: "row",
+    backgroundColor: 'purple',
+    width: Platform.OS === "web" ? 400 : (width - 24),
+    height: 40,
+    position: 'relative',
+    bottom: 30,
+    justifyContent: 'space-around'
+  },
+  cardButton: {
+    backgroundColor: 'white',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderRadius: 20,
+    minWidth: 140
+  },
+  cardButtonText: {
+    textAlign: 'center'
+  },
+  cardFooter: {
+    backgroundColor: 'pink',
+    flexDirection: "column",
+    width: Platform.OS === "web" ? 400 : (width - 24),
+    height: 100,
+  },
+  cardFooterFollowersText: {
+
+  },
+  cardFooterFollowersAvatars: {
+
+  },
+  cardFooterFollowersAvatarsImages: {
+
+  },
+  cardFooterFollowersAvatarsImagesExtra: {
+
+  },
 })
 

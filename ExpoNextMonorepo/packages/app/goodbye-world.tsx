238493ecb13dd6d8bc6cc295bcce40d38a6c15ac
lghiggino <lghiggino@gmail.com>
2021-12-05 20:41:14 -0300

    working at goodbye-world page with dripsy

--- ExpoNextMonorepo/packages/app/goodbye-world.tsx
+++ ExpoNextMonorepo/packages/app/goodbye-world.tsx
@@ -1,37 +1,40 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text } from 'dripsy'
 import { LinkToRouteName } from './utils/CustomLinks'
 
 
 export function GoodbyeWorld() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Goodbye, World!</Text>
-      <View style={styles.button}>
+    <View sx={{
+      backgroundColor: ['red', 'green'],
+      padding: [ '$2', '$3']
+    }}>
+      <Text>Goodbye, World!</Text>
+      <View>
         <LinkToRouteName routeName="hello" webpath="hello" >
-          <Text style={styles.buttonText}>Back to Index</Text>
+          <Text>Back to Index</Text>
         </LinkToRouteName>
       </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    color: 'red'
-  },
-  buttonText:{
-    color: 'white'
-  },
-  button: {
-    paddingHorizontal: 12,
-    paddingVertical: 6,
-    backgroundColor: 'red',
-    borderRadius: 6,
-    marginVertical: 5
-  }
-})
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center'
+//   },
+//   text: {
+//     color: 'red'
+//   },
+//   buttonText:{
+//     color: 'white'
+//   },
+//   button: {
+//     paddingHorizontal: 12,
+//     paddingVertical: 6,
+//     backgroundColor: 'red',
+//     borderRadius: 6,
+//     marginVertical: 5
+//   }
+// })

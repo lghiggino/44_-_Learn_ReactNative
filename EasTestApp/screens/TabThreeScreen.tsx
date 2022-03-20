3e74ea7649a43026df2fa3ca72638cb6f1ca9b08
lghiggino <lghiggino@gmail.com>
2022-03-19 21:40:33 -0300

    react native drag sort 1st commit

--- EasTestApp/screens/TabThreeScreen.tsx
+++ EasTestApp/screens/TabThreeScreen.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { View, Text } from 'react-native'
 import Draggable from 'react-native-draggable';
 
 export default function TabThreeScreen() {
+    const [aPosition, setAPosition] = useState({ x: 75, y: 100 })
+    const [bPosition, setBPosition] = useState({ x: 200, y: 300 })
+
+    const draggableB = useRef(null)
+
     return (
         <View >
             <Draggable
@@ -18,11 +23,11 @@ export default function TabThreeScreen() {
             />
             <Draggable
                 key="B"
-                x={200}
-                y={300}
+                x={bPosition.x}
+                y={bPosition.y}
                 renderColor='red'
                 renderText='B'
-                onDragRelease={(event) => { console.log(event) }}
+                onDragRelease={(event) => { console.log(event.positionX) }}
             />
             <Draggable />
             <Draggable x={50} y={50}>

6cac8755fc63f84c5218e2e450710da285d04e14
lghiggino <lghiggino@gmail.com>
2022-04-30 07:50:55 -0300

    project starter for react native with jest

---
+++ expoWithTypeScriptAndJestStarter/src/components/DataFetcher.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+
+export default function DataFetcher() {
+  const [user, setUser] = useState('');
+  const [show, setShow] = useState(false);
+
+  return (
+    <View>
+      <TextInput value={user} onChangeText={setUser} testID="input" />
+      <Button
+        title="Print Username"
+        onPress={() => {
+          // let's pretend this is making a server request, so it's async
+          // (you'd want to mock this imaginary request in your unit tests)...
+          setTimeout(() => {
+            setShow(!show);
+          }, Math.floor(Math.random() * 200));
+        }}
+      />
+      {show && <Text testID="printed-username">{user}</Text>}
+    </View>
+  );
+}
---
+++ expoWithTypeScriptAndJestStarter/src/components/__tests__/Button.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+
+import { Button } from '../Button';
+
+describe("Button test", () => {
+  it('functions as a button', () => {
+    const onPress = jest.fn();
+    const out = render(<Button onPress={onPress}>Press Me</Button>);
+
+    fireEvent.press(out.getByText('Press Me'));
+
+    expect(onPress).toBeCalled();
+  });
+})
+
+
